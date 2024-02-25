df2297cdf923d478d410cabe0833623d0d1abf96
ngmxyz <155841953+ngmxyz@users.noreply.github.com>
2024-02-25 14:22:54 -0600

    Update python_notes.py

--- python_notes.py
+++ python_notes.py
@@ -196,7 +196,50 @@ print(how_moral)
 
 # this would print 10 because 10 is the highest moral_score
 
-#####
+##### 2d lists
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+for row in matrix:
+    for item in row:
+        print(item)
+
+
+##### search in index
+
+words = ["pizza", "chips", "cookies", "bread", "beer"]
+print(("pizza" or "poison") in words)
+# would return true because pizza is in there
+
+##### append and copy in list
+
+numbers = [5, 2, 1, 5, 7, 4]
+numbers2 = numbers.copy()
+numbers.append(10)
+print(numbers)
+print(numbers2)
+
+# prints
+# [5, 2, 1, 5, 7, 4, 10]
+# [5, 2, 1, 5, 7, 4]
+
+##### remove duplicates in a list
+
+numbers = [2, 2, 4, 6, 3, 4, 6, 1]
+uniques = []
+for number in numbers:
+    if number not in uniques:
+        uniques.append(number)
+print(uniques)
+
+######
+
+
+
 
 
 
